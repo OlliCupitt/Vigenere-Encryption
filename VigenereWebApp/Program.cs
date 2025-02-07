83de3fcb00dc75b61e5e4e2e1c8424ab04e4b19e
OlliCupitt <ollicupitt1999@gmail.com>
2025-02-07 10:58:31 +0100

    procram.cs added

--- VigenereWebApp/Program.cs
+++ VigenereWebApp/Program.cs
@@ -1,35 +1,36 @@
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using VigenereWebApp;
+
 namespace VigenereWebApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            var builder = WebApplication.CreateBuilder();
+            // Register dependencies
+            builder.Services.AddSingleton<KeyGenerator>(); // Singleton since it has no state
+            builder.Services.AddTransient<VigenereCipher>(); // Transient since it processes data per request
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "My Vigenere API", Version = "v1" });
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "My Vigenere API V1");
+                options.RoutePrefix = string.Empty;
+            });
 
             app.MapControllers();
-
             app.Run();
         }
     }
