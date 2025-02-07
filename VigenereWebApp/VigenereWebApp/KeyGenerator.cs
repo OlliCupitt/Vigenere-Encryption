645658672fcaeb3f2dee0ad546cfb5aa3d164333
OlliCupitt <ollicupitt1999@gmail.com>
2025-02-07 10:14:03 +0100

    keyGenerator.cs added

---
+++ VigenereWebApp/VigenereWebApp/KeyGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace VigenereWebApp
+{
+    public class KeyGenerator
+    {
+        public string GenerateKey(string text, string key)
+        {
+            int textLength = text.Length;
+            int keyLength = key.Length;
+
+            StringBuilder repeatedKey = new StringBuilder();
+            int keyIndex = 0;
+
+            for (int i = 0; i < textLength; i++)
+            {
+                repeatedKey.Append(key[keyIndex]);
+                keyIndex = (keyIndex + 1) % keyLength;
+            }
+
+            return repeatedKey.ToString();
+        }
+    }
+
+}
